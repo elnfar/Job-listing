2a4a9a33780a603a85123bbb1c59a39a60e3cf08
Eln Far <farzaliyevelnur00@gmail.com>
2023-08-09 18:50:14 +0400

    fixed client/server components

--- app/create-job/page.tsx
+++ app/create-job/page.tsx
@@ -1,96 +1,18 @@
-'use client'
 
-import React,{ChangeEvent, useState} from 'react'
-
-import { Button } from '@/components/ui/button'
-import createJobs from '@/actions/create-actions'
-import { InputWithLabel } from '../jobs/inputJob'
+import CreateJobClient from './client'
 
 
-const initialState = {
-
-title:'',
-companyName:'',
-location:'',
-salary:'',
-type:'',
-experienceLevel:'',
-shortDescription:'',
-description:'',
-applyUrl:''
-
-}
 
 export default function page() {
 
-  const [state,setState] = useState(initialState)
 
-
-  function onChange(event:ChangeEvent<HTMLInputElement>) {
-		setState({ ...state, [event.target.name]: event.target.value });
-	}
-     
 
   return (
     <div>
-        <form action={createJobs} className='container grid grid-cols-4 gap-12'>
-
-
-            <InputWithLabel
-            type='text'
-            id='title'
-            placeholder='title'
-            label='title'
-            value={state.title}
-            onChange={onChange}
-            name='title'
-            />
-
-
-        <InputWithLabel 
-            type='text'
-            value={state.companyName}
-            id='companyName'
-            placeholder='companyName'
-            label='companyName'
-            name='companyName'
-            onChange={onChange}
-
-            />
-
-          <InputWithLabel 
-            type='text'
-
-            id='location'
-            placeholder='location'
-            label='location'
-            value={state.location}
-            onChange={onChange}
-            name='location'
-
-            />
-               <InputWithLabel 
-            type='text'
-            value={state.salary}
-            id='salary'
-            placeholder='salary'
-            label='salary'
-            onChange={onChange}
-            name='salary'
-
-            />
-
-               <InputWithLabel 
-            type='text'
-            id='type'
-            placeholder='type'
-            label='type'
-            onChange={onChange}
-            value={state.type}
-            name='type'
-            />
-        <Button type='submit' variant='outline'>Create</Button>
-        </form>
+      <div className='container text-[4rem] py-12'>
+        <h1>Create jobs</h1>
+      </div>
+      <CreateJobClient/>
     </div>
   )
 }
